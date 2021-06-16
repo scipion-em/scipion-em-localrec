116bae2b42f94bf8d4256c0d8b1bb40d00046b84
rmarabini <roberto@cnb.csic.es>
2021-06-16 12:58:03 +0200

    update version to pypy is updated

--- localrec/__init__.py
+++ localrec/__init__.py
@@ -36,7 +36,7 @@ from localrec.convert import *
 
 getXmippPath = pwem.Domain.importFromPlugin("xmipp3.base", 'getXmippPath')
 
-__version__ = '3.0.2'
+__version__ = '3.0.3'
 _logo = "localrec_logo.png"
 _references = ['Ilca2015', 'Abrishami2020']
 
